720aa22b3eaa0afa069db56a811af48f4deacdb5
Nancy <webtech.pmp@gmail.com>
2019-08-07 16:01:25 -0700

    add initial code for Header JS

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -5,12 +5,10 @@ time: String eg. "12 pm"
 */
 
 export default function Header(props) {
-  
   return (
-
-   <header class="appointment__time">
-    <h4 class="text--semi-bold">12pm</h4>
-    <hr class="appointment__separator" />
-  </header>
-  )
+    <header class="appointment__time">
+      <h4 class="text--semi-bold">12pm</h4>
+      <hr class="appointment__separator" />
+    </header>
+  );
 }
