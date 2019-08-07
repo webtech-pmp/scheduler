6a2c9417dd32f9d74887ede7d085b6743a5b7e48
Nancy <webtech.pmp@gmail.com>
2019-08-06 18:17:43 -0700

    add initial base code for InterviewerList JS

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import InterviewerListItem from "components/InterviewerListItem";
+
+export default function InterviewerList(prop) {
+  return (
+    <section class="interviewers">
+      <h4 class="interviewers__header text--light">Interviewer</h4>
+      <ul class="interviewers__list" />
+    </section>
+  );
+}
