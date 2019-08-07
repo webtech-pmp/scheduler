2167d2c913968f6080729126b6f86e1938e0de2d
Nancy <webtech.pmp@gmail.com>
2019-08-06 20:36:10 -0700

    working InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,11 +2,28 @@ import React from "react";
 
 import InterviewerListItem from "components/InterviewerListItem";
 
-export default function InterviewerList(prop) {
+/* PROPS
+interviewers: Array a list of interviewers
+interviewer: current selected interviewer
+setInterviewer: Function
+*/
+
+export default function InterviewerList(props) {
   return (
     <section class="interviewers">
       <h4 class="interviewers__header text--light">Interviewer</h4>
-      <ul class="interviewers__list" />
+      <ul class="interviewers__list">
+        {props.interviewers.map(interviewer => {
+          return (
+            <InterviewerListItem
+              name={interviewer.name}
+              avatar={interviewer.avatar}
+              selected={props.value === interviewer.id}
+              setInterviewer={() => props.setInterviewer(interviewer.name)}
+            />
+          );
+        })}
+      </ul>
     </section>
   );
 }
