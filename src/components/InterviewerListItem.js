d30211d65ef8e9d17317056d965c6f231d7986e7
Nancy <webtech.pmp@gmail.com>
2019-08-06 20:36:53 -0700

    working InterviewerListItem JS

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,15 +3,25 @@ import React from "react";
 import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
+/* PROPS
+id: interviewer id (as number)
+name:String the name of the interviewer
+avatar: url for image (as string)
+selected:Boolean true or false declaring that this interviewer is selected
+*/
+
 export default function InterviewerListItem(props) {
+  const InterviewerListClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
   return (
-    <li class="interviewers__item">
+    <li className={InterviewerListClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {<span>{props.selected ? props.name : ""} </span>}
     </li>
   );
 }
