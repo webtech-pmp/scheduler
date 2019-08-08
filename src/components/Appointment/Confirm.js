be2e093c75778b1d829145657ee9edd47ca8f802
Nancy <webtech.pmp@gmail.com>
2019-08-07 17:22:52 -0700

    add Confirm code JS

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Button from "components/Button";
+
 /* PROPS
 message:String eg. "Delete the appointment?"
 onConfirm:Function to be called when the user clicks the Confirm button
@@ -8,5 +10,16 @@ onCancel:Function to be called when the user clicks the Cancel button
 
 export default function Empty(props) {
   return (
-  )
+    <main className="appointment__card appointment__card--confirm">
+      <h1 className="text--semi-bold">{props.message}</h1>
+      <section className="appointment__actions">
+        <Button danger onClick={props.onCancel}>
+          Cancel
+        </Button>
+        <Button danger onClick={props.onConfirm}>
+          Confirm
+        </Button>
+      </section>
+    </main>
+  );
 }
