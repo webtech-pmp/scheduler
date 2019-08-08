4f5b554fe6e0cb6d92c6a97d4fcb6c9b6503e1b0
Nancy <webtech.pmp@gmail.com>
2019-08-07 20:41:42 -0700

    import Fragment + add Appointment EMPTY

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -216,4 +216,11 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
       onSave={action("onSave")}
     />
+  ))
+  // Appointment EMPTY
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
   ));
