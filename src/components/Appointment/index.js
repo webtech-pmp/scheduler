55562eff2914c92a5171b0ff4fc890418ab69dca
Nancy <webtech.pmp@gmail.com>
2019-08-08 13:46:22 -0700

    add check for empty appointment elements

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,11 +2,28 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
+
 import classNames from "classnames";
 
+/* PROPS
+id: key
+time: time
+*/
+
 export default function Appointment(props) {
-  const AppointmentClass = classNames();
   return (
-    <article className="appointment"></article>
+    <article className="appointment" key={props.id}>
+      <Header time={props.time} />
+      {!props.interview && <Empty />}
+      {props.interview && 
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+        />}
+    </article>
   );
 }
