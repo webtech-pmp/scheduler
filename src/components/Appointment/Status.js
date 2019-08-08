f4002d379fc67dbee2977e92056d80abc1161ad5
Nancy <webtech.pmp@gmail.com>
2019-08-07 17:29:43 -0700

    Status initial code

---
+++ src/components/Appointment/Status.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+/* PROPS
+message:String eg. "Deleting"
+*/
+
+export default function Status(props) {
+  return (
+    <main class="appointment__card appointment__card--status">
+      <img
+        class="appointment__status-image"
+        src="images/status.png"
+        alt="Loading"
+      />
+      <h1 class="text--semi-bold">Deleting</h1>
+    </main>
+  );
+}
