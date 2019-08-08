90358aa754cc47ba1373dae8c19ddd43562e0a60
Nancy <webtech.pmp@gmail.com>
2019-08-07 17:11:38 -0700

    initial code for Confirm JS

---
+++ src/components/Appointment/Confirm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/* PROPS
+message:String eg. "Delete the appointment?"
+onConfirm:Function to be called when the user clicks the Confirm button
+onCancel:Function to be called when the user clicks the Cancel button
+*/
+
+export default function Empty(props) {
+  return (
+  )
+}
