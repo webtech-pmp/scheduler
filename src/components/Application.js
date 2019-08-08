c23c329817773339d44674103962ed1406f198a1
Nancy <webtech.pmp@gmail.com>
2019-08-08 16:58:20 -0700

    add appointment scheduling

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,37 +3,14 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 
+import Appointment from "components/Appointment";
 
+/* PROPS
+id: number
+time: string
+interview: object with student and interviewer object with name
 
-export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        {" "}
-        {/* Replace this with the sidebar elements during the "Environment Setup" activity. */}{" "}
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={day => setDay(day)} />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-      </section>{" "}
-      <section className="schedule">
-        {" "}
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}{" "}
-      </section>{" "}
-    </main>
-  );
-}
+*/
 
 const days = [
   {
@@ -52,3 +29,73 @@ const days = [
     spots: 0
   }
 ];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm"
+  },
+  {
+  id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm"
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Victoria Rory",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  }
+];
+
+export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+  return (
+    <main className="layout">
+      <section className="sidebar">
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={days} day={day} setDay={day => setDay(day)} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+      </section>
+      <section className="schedule">
+       {appointments.map(appointment => {
+         return (
+            <Appointment 
+              key={appointment.id} {...appointment} />
+         )
+       })}
+        <Appointment id="last" time="4pm" />
+      </section>
+    </main>
+  );
+}
+
+
