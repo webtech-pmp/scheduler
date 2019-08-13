3a45441ec09f013eb09a8055bc056fdf5f1745fa
Nancy <webtech.pmp@gmail.com>
2019-08-12 17:16:20 -0700

    add getInterview()

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,13 +1,24 @@
 export function getAppointmentsForDay(state, day) {
   let result = [];
-  let appointmentArray = '';
+  let appointmentArray = "";
   for (let appointmentDay of state.days) {
     if (appointmentDay.name === day) {
-      appointmentArray = appointmentDay.appointments
-      for (let appNumber of appointmentArray) {
-        result.push(state.appointments[appNumber])
+      appointmentArray = appointmentDay.appointments;
+      for (let appointmentNumber of appointmentArray) {
+        result.push(state.appointments[appointmentNumber]);
       }
     }
   }
   return result;
+}
+
+export function getInterview(state, interview) {
+  const interview_Id = interview.interviewer;
+  const interviewAppointment = { ...interview };
+  if (interview) {
+    interviewAppointment.interviewer = state.interviewers[interview_Id];
+    return interviewAppointment;
+  } else {
+    return null;
+  }
 }
