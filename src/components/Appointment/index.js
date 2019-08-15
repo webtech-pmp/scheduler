710a40ade2523105658e2431016fa1e6a9bf40d2
Nancy <webtech.pmp@gmail.com>
2019-08-14 21:14:00 -0700

    add CREATE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,3 +1,4 @@
+// Dependencies
 import React from "react";
 
 import "components/Appointment/styles.scss";
@@ -8,22 +9,44 @@ import Show from "components/Appointment/Show";
 import Form from "components/Appointment/Form";
 
 import classNames from "classnames";
+import useVisualMode from "hooks/useVisualMode";
 
 /* PROPS
 id: key
 time: time
 */
 
+//CONST
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment" key={props.id}>
       <Header time={props.time} />
-      {!props.interview && <Empty />}
-      {props.interview && 
-      <Show
-        student={props.interview.student}
-        interviewer={props.interview.interviewer}
-        />}
+
+      {mode === EMPTY && (
+        <Empty
+          onAdd={() => {
+            transition(CREATE);
+          }}
+        />
+      )}
+
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+
+      {mode === CREATE && <Form interviewers={[]} />}
     </article>
   );
 }
