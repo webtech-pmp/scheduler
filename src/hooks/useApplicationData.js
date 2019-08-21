69a9f4a833c7ac02eb37664f6fa2efb9d34bd1d5
Nancy <webtech.pmp@gmail.com>
2019-08-20 20:03:31 -0700

    change useState to useReducer + add CONST

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,11 @@
-import React, {useState, useEffect } from "react";
+// Dependencies
+import React, {useReducer, useEffect } from "react";
 import axios from "axios";
+
+// Const
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
 
 
 export default function useApplicationData() {
@@ -9,6 +15,7 @@ export default function useApplicationData() {
   // The setDay action can be used to set the current day.
   // The bookInterview action makes an HTTP request and updates the local state.
   // The cancelInterview action makes an HTTP request and updates the local state.
+
 
   const [state, setState] = useState({
     day: "Monday",
