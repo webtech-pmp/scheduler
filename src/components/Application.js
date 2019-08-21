3ecf2279fd76c131757906d9802319fc194996c8
Nancy <webtech.pmp@gmail.com>
2019-08-21 15:59:16 -0700

    correct typo

--- src/components/Application.js
+++ src/components/Application.js
@@ -64,7 +64,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {appointments}
-        <Appointment id="last" time="4pm" />
+        <Appointment id="last" time="5pm" />
       </section>
     </main>
   );
