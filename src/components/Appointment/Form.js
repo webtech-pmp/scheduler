d589e48e8884a79598ea75111b8c0a33a0c94078
Nancy <webtech.pmp@gmail.com>
2019-08-23 17:28:06 -0700

    add input validator () onClick

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Button from "components/Button";
 
 import InterviewerList from "components/InterviewerList";
+import useVisualMode from "hooks/useVisualMode";
 
 /* PROPS
 name:String
@@ -11,16 +12,34 @@ interviewer:Number
 onSave:Function
 onCancel:Function
 */
+const SHOW = "SHOW";
 
 export default function Form(props) {
+  const { mode, transition, back } = useVisualMode(SHOW);
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
     setInterviewer(null);
     props.onCancel();
   };
+
+  // Input error check
+  function validate() {
+    if (!name) {
+      setError("Student name cannot be blank. Please enter a name.");
+      return;
+    }
+    if(!interviewer) {
+      setError("An interviewer must be selected. Please select an interviewer.");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -34,6 +53,7 @@ export default function Form(props) {
             onChange={event => setName(event.target.value)}
           />
         </form>
+        {error && <p>({error})</p>}
         <InterviewerList
           interviewers={props.interviewers}
           interviewer={interviewer}
@@ -41,13 +61,16 @@ export default function Form(props) {
         />
       </section>
       <section className="appointment__card-right">
+        
         <section className="appointment__actions">
           <Button danger onClick={reset}>
             Cancel
           </Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>
-            Save
-          </Button>
+          <Button confirm onClick={() => // validate input field prior save
+            validate()
+            //props.onSave(name, interviewer)
+          }>
+          Save</Button>
         </section>
       </section>
     </main>
